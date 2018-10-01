55a76acf2b1d6dfede28182db646d943fd77cb7c
Arthur Ngondo <ngondoarthur@gmail.com>
2018-10-01 13:47:55 +0300

    [Chore  #160885098] Add function stubs for normal user operations

---
+++ postit.py
@@ -0,0 +1,17 @@
+""" Postit.py """
+
+
+class UsersOps():
+    pass
+
+class ModeratorOps(UsersOps):
+    pass
+
+class AdminOps(ModeratorOps):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ test_postit.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestUserOps(unittest.TestCase):
+    def test_users_can_log_in(self):
+        pass
+    def test_users_can_log_out(self):
+        pass
+    def test_for_timestamp_when_user_logs_in(self):
+        pass
+    def test_that_users_can_edit_their_own_comments(self):
+        pass
+
+    def test_that_users_canot_edit_other_users_comments(self):
+        pass
+
+    def test_users_cannot_delete_any_comments(self):
+        pass
+
