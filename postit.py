a038773765abb0b11b9a821f044db510137893ef
Arthur Ngondo <ngondoarthur@gmail.com>
2018-10-01 14:44:34 +0300

    [Chore  #160885098] Add function stubs

--- postit.py
+++ postit.py
@@ -1,8 +1,39 @@
-""" Postit.py """
+""" Postit.py 
+all_comments = {
+    int : {
+        "author" : str,
+        "message" : str,
+        "datetime" : str
+    }
+}
+"""
+import datetime
 
+all_posts = {}
 
 class UsersOps():
-    pass
+    def __init__(self):
+        self.login_status = False
+        self.logged_in_at = None      
+
+    def login(self, username, password):
+        # simple regex
+
+        self.login_status = True
+        self.logged_in_at = datetime.datetime.now()
+        print('YOU ARE LOGGED IN')
+
+    
+    def logout(self):
+        pass
+
+    def post_comment(self):
+        pass
+    
+    def edit_own_comment(self):
+        pass
+
+
 
 class ModeratorOps(UsersOps):
     pass
@@ -11,7 +42,12 @@ class AdminOps(ModeratorOps):
     pass
 
 def main():
-    pass
+    new_username = input('Enter username :')
+    new_password = input('Enter password :')
+
+    samle_user = UsersOps()
+    samle_user.login(new_username, new_password)
+
 
 if __name__ == '__main__':
     main()
